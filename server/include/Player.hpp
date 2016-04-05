4dca7482026e04489c834cede8f1a9d002e4273b
Randall <takmo@bitwisehero.com>
2016-04-05 11:47:51 -0500

    Modified Player a bit. Kinda.

--- server/include/Player.hpp
+++ server/include/Player.hpp
@@ -1,18 +1,25 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include <string>
 #include "Connection.hpp"
+
+#include <string>
 
 using namespace std;
 
-class Player {
+class Player
+{
     public:
-        Player(int _player_id, int _game_id, string _name);
-        ~Player();
+        Player(int _player_id, int _game_id, string _name, Connection *_conn);
+
+        // Getters
         int get_player_id() { return player_id; }
         int get_game_id() { return game_id; }
         string get_name() { return name; }
+        Connection *get_connection() { return connection; }
+
+        // Setters
+        void set_game_id(int _game_id) { game_id = _game_id; }
     
     private:
         int player_id;
--- server/src/Player.cpp
+++ server/src/Player.cpp
@@ -1,11 +1,9 @@
 #include "Player.hpp"
 
-Player::Player(int _player_id, int _game_id, string _name) {
+Player::Player(int _player_id, int _game_id, string _name, Connection *_conn)
+{
     player_id = _player_id;
     game_id = _game_id;
     name = _name;
-}
-
-Player::~Player(){
-    
+    connection = _conn;
 }
