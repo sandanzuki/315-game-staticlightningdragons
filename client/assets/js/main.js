7fa730dad675b0b5369b7c509c3f1aabee939b11
phelangood72 <phelangood72@gmail.com>
2016-04-14 21:44:42 -0500

    Minor changes

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,10 +1,10 @@
-var connection = new WebSocket("ws://sld.bitwisehero.com:13337", "rqs");
-
-
 // declare and initialize game
 var game = game || {};
 game.win = true;
+connection = new WebSocket("ws://pulse.bitwisehero.com:13337", "rqs");
+
 game = new Phaser.Game(900, 600, Phaser.AUTO,'');
+
 
 // states of game
 game.state.add('Menu', Menu);
@@ -14,3 +14,4 @@ game.state.add('Select', Select);
 game.state.add('Game', Game);
 game.state.add('GameOver', GameOver);
 game.state.start('Menu'); // start on 'Menu' state
+
