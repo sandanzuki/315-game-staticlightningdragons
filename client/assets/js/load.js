3b35ac6f4482907c257defc4b200c60d888869c1
Eric Chung <eric.chung2718@gmail.com>
2016-04-24 17:15:31 -0500

    load.js now supports animation

--- client/assets/js/load.js
+++ client/assets/js/load.js
@@ -1,17 +1,40 @@
+var wait1, wait2, wait3, wait4,
+    counter_waiter = 0;
+    
 var Load = {
     preload : function() {
-        game.load.image('load', './assets/images/load.png');
-        
+        game.load.image('wait1', './assets/images/wait1.png');
+        game.load.image('wait2', './assets/images/wait2.png');
+        game.load.image('wait3', './assets/images/wait3.png');
+        game.load.image('wait4', './assets/images/wait4.png');
+
         // align canvas
         game.scale.pageAlignHorizontally = true;
         game.scale.pageAlignVertically = true;
-        var connection;
     },
 
     create : function() {
-        // TODO change this to depend on player connecting.
-        // that is, when another player is connected, it is
-        // done "loading" and can go on to the next state
-        pic = this.add.sprite(0, 0, 'load'); // add background
-    }
+        wait4 = this.add.sprite(0, 0, 'wait4'); // add background
+        wait3 = this.add.sprite(0, 0, 'wait3'); // add background
+        wait2 = this.add.sprite(0, 0, 'wait2'); // add background
+        wait1 = this.add.sprite(0, 0, 'wait1'); // add background
+
+        game.time.events.loop(Phaser.Timer.SECOND * 0.5, this.change_frame_wait, this);
+    },
+
+    change_frame_wait : function() {
+        if(counter_waiter == 0)
+            wait1.visible = !wait1.visible;
+        if(counter_waiter == 1)
+            wait2.visible = !wait2.visible;
+        if(counter_waiter == 2)
+            wait3.visible = !wait3.visible;
+        if(counter_waiter == 3) {
+            wait3.visible = !wait3.visible;
+            wait2.visible = !wait2.visible;
+            wait1.visible = !wait1.visible;
+            counter_waiter = -1;
+        }
+        counter_waiter++;
+    },
 };
